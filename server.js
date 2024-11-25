189ca84983f195154f14cb8842e10664f56c5012
acebird <acebird15@gmail.com>
2024-11-24 21:15:07 -0500

    Create product connection

--- server.js
+++ server.js
@@ -9,7 +9,45 @@ app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
 
 
-app.get("/CreateShoppingCart", function(req, res) {
+app.get("/CreateProduct", function(req, res) {
+    try {
+        var mongodb = require('mongodb');
+        var MongoClient = mongodb.MongoClient;
+            res.header("Access-Control-Allow-Origin", "*");
+            if(!req.query.bookTitle) {
+                return res.send({"result": "missing the Book Title"});
+            } else {
+                var product = {
+                    "Product ID": req.query.productid,
+                    "Product Description": req.query.productdescription,
+                    "Product Category": req.query.productcategory,
+                    "Measure": req.query.measure,
+                    "Product Price": req.query.productprice,
+                    "Weight": req.query.productweight
+                    
+                }
+    
+                var url = 'mongodb://localhost:27017';
+                MongoClient.connect(url, function (err, client) {
+                    if (err) {
+                        return res.send({"result" : "failed"});
+                      }  else {
+                        var db = client.db('StoreFront');
+                        var collection = db.collection('products');
+                            collection.insertOne (cart, function(err, res) {
+                                if (err) throw err;
+                                client.close();
+                            });
+                            return res.send (cart);
+                        }
+                    });
+                }
+            } catch (error) {
+                console.error(error);
+            }
+});
+
+app.get("/CreateShipping", function(req, res) {
     try {
         var mongodb = require('mongodb');
         var MongoClient = mongodb.MongoClient;
@@ -41,7 +79,7 @@ app.get("/CreateShoppingCart", function(req, res) {
             } catch (error) {
                 console.error(error);
             }
-    });
+});
 
 const PORT = 3000;
 server.listen(PORT, () => {
